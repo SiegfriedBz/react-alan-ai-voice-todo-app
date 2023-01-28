79d656ee8ac884e62a92d406b226275409b44ed9
Siegfried <siegfried.bozza@renuo.ch>
2023-01-28 18:21:17 +0100

    clean up

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ const Navbar = () => {
     return(
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Alan AI</a>
+                <a className="navbar-brand fs-3 fw-bold" href="/">Voice Todo</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -12,12 +12,6 @@ const Navbar = () => {
                     <ul className="navbar-nav">
                         <li className="nav-item">
                             <a className="nav-link active" aria-current="page" href="/">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Courses</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Notes</a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="#">About</a>
